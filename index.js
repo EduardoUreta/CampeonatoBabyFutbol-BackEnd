ed3f4dcaa691482d35b162dbae60dc90e2a40eeb
Eduardo Ureta <eduardo.uretaca@gmail.com>
2025-03-27 20:35:28 -0300

    Se modifican ENVs

--- index.js
+++ index.js
@@ -3,6 +3,9 @@ import express from "express";
 import { EquiposRoutes, EstadisticasRoutes, JugadoresRoutes, PartidosRoutes, SessionsRoutes, UsuariosRoutes } from "./routes/index.js";
 import { errorHandler } from "./middlewares/index.js";
 import cors from "cors";
+import path from "path";
+import { Sequelize } from "./models/index.cjs";
+import { envConfig } from "./config/config.cjs";
 
 const app = express();
 const port = 3000;
@@ -17,7 +20,6 @@ app.use(cors({
     origin: 'http://localhost:5173',
     credentials: true, 
 }));
-  
 
 app.use("/api/jugador", JugadoresRoutes);
 app.use("/api/equipo", EquiposRoutes);
@@ -27,6 +29,23 @@ app.use("/api/usuario", UsuariosRoutes);
 app.use("/api/auth", SessionsRoutes);
 
 app.use(errorHandler);
+
+app.use("/*", (req, res) => {
+    res.sendFile(path.join(process.cwd(),"public/index.html"));
+});
+
+const sequelize = new Sequelize(envConfig.database, envConfig.username, envConfig.password, {
+    host: envConfig.host,
+    dialect: envConfig.dialect
+});
+
+sequelize.authenticate()
+  .then(() => {
+    console.log('Conexión a la base de datos exitosa.');
+  })
+  .catch(err => {
+    console.error('No se pudo conectar a la base de datos:', err);
+  });
 
 app.listen(port, () => {
     console.log(`Servidor en puerto ${port}`);
